fe229ea523af7029a8c3367a70acf2facffb86cb
zhux <419514268@qq.com>
2020-01-12 21:17:32 +0800

    修改输出

--- 2_2_1.cpp
+++ 2_2_1.cpp
@@ -4,11 +4,11 @@
 #include <iostream>
 using namespace std;
  int main(){
-     cout << "The size of an int is:\t\t" << sizeof(int) << "bytes.\n";
-     cout << "The size of an int is:\t\t" << sizeof(short) << "bytes.\n";
-     cout << "The size of an int is:\t\t" << sizeof(long) << "bytes.\n";
-     cout << "The size of an int is:\t\t" << sizeof(char) << "bytes.\n";
-     cout << "The size of an int is:\t\t" << sizeof(float) << "bytes.\n";
-     cout << "The size of an int is:\t\t" << sizeof(double) << "bytes.\n";
+     cout << "The size of an int is:\t\t" << sizeof(int) << " bytes.\n";
+     cout << "The size of an short is:\t\t" << sizeof(short) << " bytes.\n";
+     cout << "The size of an long is:\t\t" << sizeof(long) << " bytes.\n";
+     cout << "The size of an char is:\t\t" << sizeof(char) << " bytes.\n";
+     cout << "The size of an float is:\t\t" << sizeof(float) << " bytes.\n";
+     cout << "The size of an double is:\t\t" << sizeof(double) << " bytes.\n";
      return 0;
  }
