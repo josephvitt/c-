feaeefd5f27300ca6c6b79940398a7eef9afc587
josephvitt <419514268@qq.com>
2020-01-16 15:11:21 +0800

    UML

---
+++ 4_7_1.cpp
@@ -0,0 +1,17 @@
+/*
+UML：可视化的面向对象的建模语言
+三个基本部分：
+1.事物（Things）
+2.关系（Relationships）
+3.图 （Diagrams）
+
+依赖关系 类A---------->类B:类A使用类B,或者类A依赖类B
+关联关系 
+包含关系 聚集；整体与部分的关系
+
+*/
+#include <iostream>
+using namespace std;
+int main(){
+    return 0;
+}
