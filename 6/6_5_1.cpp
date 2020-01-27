cdb8c230ff3b8916c2822bd7ee3d662c9a6b4f61
zhux <419514268@qq.com>
2020-01-27 16:55:43 +0800

    基于范围的for循环

---
+++ 6/6_5_1.cpp
@@ -0,0 +1,21 @@
+/*基于范围的for循环*/
+#include <iostream>
+int main(){
+    /*int array[3] = {1,2,3};
+    //定义指针首地址赋值到指针
+    int *p;
+    for (p = array; p < array + sizeof(array) / sizeof(int); p++)
+    {
+        *p += 2;
+        std::cout << *p << std::endl;
+    }
+    */
+    int array[3] = {1,2,3};
+    for(int &e : array){
+        e+=2;
+        std::cout << e << std::endl;
+    }
+
+
+    return 0;
+}
