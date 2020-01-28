84f3fa0fc04d37fa608733866a1c4ea1f891bb84
zhux <419514268@qq.com>
2020-01-28 13:56:24 +0800

    一维数组的应用举例

--- 6/6_2_3.cpp
+++ 6/6_2_3.cpp
@@ -3,6 +3,7 @@
 
 1.循环从键盘读入若干组选择题答案，计算并且输出每组答案的正确率，直到输入ctrl+z为止。
 2.每组连续输入5个答案，每个答案可以是‘a’...'d'
+
 */
 #include <iostream>
 using namespace std;
@@ -31,6 +32,6 @@ int main(){
             cout << endl;
         }
     }
-    
+
     return 0;
 }
