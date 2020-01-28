02fea2e5c414410a9ed4b496b139e9b1936a3130
zhux <419514268@qq.com>
2020-01-28 13:39:53 +0800

    实验六

--- 6/6_8_2.cpp
+++ 6/6_8_2.cpp
@@ -3,9 +3,11 @@
 using namespace std;
 
 int main(){
+    /*
     Employee e("Wang Er","Haidian","Beijing","100084");
     e.display();
     e.change_name("Li San");
     e.display();
     return 0;
+    */
 }
