4cb7d99a3afb651cd05a47b7a0e8a79432593537
zhux <419514268@qq.com>
2020-01-29 22:18:46 +0800

    基于范围的for循环配合auto

---
+++ 6/62.6.1.cpp
@@ -0,0 +1,23 @@
+/*
+基于范围的for循环配合auto
+*/
+#include <iostream>
+#include <vector>
+using namespace std;
+
+int main(){
+    /*
+    vector<int> v = {1,2,3};
+    for (auto i = v.begin(); i != v.end(); ++i)
+    {
+        cout << *i << endl;
+    }
+
+    for(auto e : v){
+        cout << e << endl;
+    }
+    */
+
+    
+    return 0;
+}
