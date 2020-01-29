bb41c3409ed564da4c4622f7f247fe54e047f3ea
zhux <419514268@qq.com>
2020-01-29 22:17:56 +0800

    VECTOR对象

--- 6/62.6.cpp
+++ 6/62.6.cpp
@@ -3,7 +3,6 @@ VECTOR对象
 1.封装任何类型的动态数组，自动创建和删除
 2.数组下标越界检查。
 
-
 定义：
 vector<元素类型> 数组名对象名(数组长度)
 
@@ -20,8 +19,30 @@ vector对象名.size()
 
 */
 #include <iostream>
+#include <vector>
 using namespace std;
+
+//计算数组arr中元素的平均值
+double average(const vector<double> &arr){
+    double sum = 0;
+    for(unsigned i = 0;i<arr.size();i++){
+        sum += arr[i];
+    }
+    return sum/arr.size();
+}
+
 int main(){
+    unsigned n;
+    cout << "n= ";
+    cin >> n;
+
+    vector<double> arr(n);//创建数组对象
+    cout << "Please input " << n << " real numbers:" << endl;
+    for(unsigned i = 0;i<n;i++){
+        cin >> arr[i];
+    }
+
+    cout << "Average = " << average(arr) << endl;
 
     return 0;
 }
