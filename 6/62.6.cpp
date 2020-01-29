118d0e74ebd3060b24a8ba57c2c0f1bb7cd251d0
zhux <419514268@qq.com>
2020-01-29 17:31:50 +0800

    VECTOR对象

---
+++ 6/62.6.cpp
@@ -0,0 +1,27 @@
+/*
+VECTOR对象
+1.封装任何类型的动态数组，自动创建和删除
+2.数组下标越界检查。
+
+
+定义：
+vector<元素类型> 数组名对象名(数组长度)
+
+vector<int> arr(5)
+建立大小为5的int数组
+
+对象的使用
+1.对数组元素的引用，与普通数组具有相同的形式
+vector对象名 [下标表达式]
+vector数组对象名不表示数组首地址
+2.获得数组长度
+用size函数
+vector对象名.size()
+
+*/
+#include <iostream>
+using namespace std;
+int main(){
+
+    return 0;
+}
