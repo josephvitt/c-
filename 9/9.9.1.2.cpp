a9cc11f672d2a9e567494a224449a78287fc66d7
zhux <419514268@qq.com>
2020-02-03 23:22:55 +0800

    二分查找（折半查找）

---
+++ 9/9.9.1.2.cpp
@@ -0,0 +1,22 @@
+/*
+查找
+1，二分查找（折半查找）
+*/
+template<class T>
+int binSearch(const T list[],int n,const T &key){
+    int low = 0;
+    int high = n-1;
+    while ((low <= high))
+    {
+        int mid = (low+high) /2;
+        if(key == list[mid])
+            return mid;
+        else if(key < list[mid])
+        {
+            high = mid -1;
+        } else {
+            low = mid +1;
+        }
+    }
+    return -1;
+}
