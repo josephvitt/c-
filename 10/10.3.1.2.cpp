1e4a97019216047444f00f3abca1313a54a0230d
zhux <419514268@qq.com>
2020-02-05 14:36:20 +0800

    综合运用几种迭代器的示例

---
+++ 10/10.3.1.2.cpp
@@ -0,0 +1,32 @@
+/*
+综合运用几种迭代器的示例
+*/
+#include <algorithm>
+#include <iterator>
+#include <iostream>
+#include <vector>
+using namespace std;
+
+//将来自输入迭代器的n个T类型的数值排序，将结果通过输出迭代器result输出
+template<class T,class InputIterator,class OutputIterator>
+void mySort(InputIterator first,InputIterator last,OutputIterator result){
+    //通过输入迭代器将输入数据存入向量容器s中
+    vector<T> s;
+    for(;first != last;++first){
+        s.push_back(*first);
+    }
+    //对s进行排序，sort函数的参数必须是随机访问迭代器
+    sort(s.begin(),s.end());
+    copy(s.begin(),s.end(),result);
+}
+
+int main(){
+    double a[5] = {1.2,2.4,0.8,3.3,3.2};
+    mySort<double>(a,a+5,ostream_iterator<double>(cout," "));
+    cout << endl;
+    //从标准输入读入若干个整数，将排序后的结果输出
+    mySort<int>(istream_iterator<int>(cin),istream_iterator<int>(),
+    ostream_iterator<int>(cout," "));
+    cout << endl;
+    return 0;
+}
